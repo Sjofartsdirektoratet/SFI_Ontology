f7ca75362b3e4e526a653993b9fe468f22144c3f
Stian Teien <stian.teien@capgemini.com>
2021-06-22 12:57:30 +0200

    lagt til viz

---
+++ src/Viz.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import * as d3 from "d3";
+
+
+function Viz() {
+    return(
+        <h1>
+            Min første overskrift
+        </h1> 
+        
+
+    )
+}
+
+export default Viz;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Viz from './Viz';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Viz />
   </React.StrictMode>,
   document.getElementById('root')
 );
