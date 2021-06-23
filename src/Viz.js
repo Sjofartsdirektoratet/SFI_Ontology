8a99ea4072068e8f6a3bd77b39b607add3279f41
Mathisen <andreas.mathisen@capgemini.com>
2021-06-23 12:43:40 +0200

    Viz file push

--- src/Viz.js
+++ src/Viz.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import * as d3 from "d3";
-
+import React, {useEffect,useState} from 'react';
+import './App.css';
+import {Tree, treeUtil} from 'react-d3-tree';
+import * as d3 from 'd3';
+ 
 
 function Viz() {
+    const [data, setData] = useState([]);
+    
+    useEffect(() => {
+        d3.csv('SFI_csv.csv').then(data => {
+            setData(data);
+        });
+    }, []);
+
+    
+    
     return(
-        <h1>
-            Min første overskrift
-        </h1> 
         
-
-    )
+        <div id="treeWrapper" style={{width: '1000px', height: '1000px'}}>
+            
+          <h2></h2>
+          
+   
+        </div>
+      );
 }
-
 export default Viz;
+
+
+ 
+
+ 
