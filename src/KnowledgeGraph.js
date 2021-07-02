14952c48a00ecf0075fd468a7e477f09d1d3eea3
Mathisen <andreas.mathisen@capgemini.com>
2021-07-02 15:48:52 +0200

    Work in progress

--- src/KnowledgeGraph.js
+++ src/KnowledgeGraph.js
@@ -30,28 +30,17 @@ const KnowledgeGraph = () => {
         return i;
     }
 
-    const collapseLevel = (d,node) => {
-        
-        if (node.children && node.depth > 1) {
-            
-            node._children = node.children;
-            node._children.forEach(collapseLevel);
-            node.children = null;
-        } else if (node.children) {
-            node.children.forEach(collapseLevel);
-        }
-    }
+    
 
-    const [fromJson, setFromJson] = useState(0);
-    d3.json('jsontest_2.json').then(data => {
-        setFromJson(data)
-    })
+    //const [fromJson, setFromJson] = useState(0);
+
+    
     
     useEffect(() => {
-        if(fromJson){
-            console.log(fromJson)
+        d3.json('jsontest_2.json').then(data => {
+            
             var data2 = [] // New array to store triples with code property to avoid multiple root problem
-            for (const elem of fromJson) {
+            for (const elem of data) {
                     if(elem["https://www.sdir.no/SFI-model#code"]){
                         data2.push(elem)
                     }
@@ -73,24 +62,23 @@ const KnowledgeGraph = () => {
                 })
             (data2);
 
-            let height = 2500
-            let width = 2500
-            let radius = width/2 - 50
+            let height2 = 2500
+            let width2 = 2500
+            let radius = width2/2 - 50
 
-            let tree = d3.tree()
-            .size([2 * Math.PI, radius]) 
-            .separation((a, b) => (a.parent == b.parent ? 1 : 6)/ a.depth)
+            // let tree = d3.tree()
+            // .size([2 * Math.PI, radius]) 
+            // .separation((a, b) => (a.parent == b.parent ? 1 : 6)/ a.depth)
 
             
             //console.log(hierarchy)
             
 
-            const root = tree(hierarchy)
-            
+           
             var svg = d3.select(d3KGraph.current)
                 .append('svg')
-                .attr("height",height)
-                .attr("width",width)
+                .attr("height",height2)
+                .attr("width",width2)
                 
             
 
@@ -99,100 +87,143 @@ const KnowledgeGraph = () => {
                 .attr("class", "tooltip")	
                 .attr("id", "tooltips")
                 
-
+            console.log(hierarchy)
             
-            // Applying attributes to Links in graph
-            svg.append("g") 
-                    .attr('transform', "translate("+(width/2)+","+(height/2)+")")
-                    .attr("fill", "none")
-                    .attr("stroke-opacity", 0.5)
-                    .attr("stroke-width", 1)
-                .selectAll("path")
-                .data(root.links())
-                .join("path")
-                    .attr("d", d3.linkRadial()
-                        .angle(d => d.x)
-                        .radius(d => d.y))
+            
+            const update = (data) => {
+                
+               
+                const nodes =  data.copy().count().value
+                let height = nodes/3 + 1000
+                let width = nodes/3 + 1000
+                let radius = width/2 - 50
+
+                let tree = d3.tree()
+                .size([2 * Math.PI, radius]) 
+                .separation((a, b) => (a.parent == b.parent ? 1 : 12)/ a.depth)
+                
+
+                // data.sort(function(a,b){
+                //     return d3.descending(parseInt(a.data["https://www.sdir.no/SFI-model#code"][0]["@value"][0]),parseInt(b.data["https://www.sdir.no/SFI-model#code"][0]["@value"][0]))
+                // })
+                
+                const root = tree(data)
+                console.log(nodes)
+
+
+                    // Applying attributes to Links in graph
+                svg.append("g")
+                        .attr('transform', "translate("+(width2/2)+","+(height2/2)+")")
+                        .attr("fill", "none")
+                        .attr("stroke-opacity", 0.5)
+                        .attr("stroke-width", 1)
+                    .selectAll("path")
+                    .data(root.links())
+                    .join("path")
+                        .attr("d", d3.linkRadial()
+                            .angle(d => d.x)
+                            .radius(d => d.y))
+                        
+                        .attr('stroke', function(d) { return applyColor(d.source); });
+
+                
+                // Applying attributes to Nodes in graph
+                svg.append("g")
+                    .attr('transform', "translate("+(width2/2)+","+(height2/2)+")")
+                    .selectAll("circle")
+                    .data(root.descendants())
+                    .join("circle")
+                        .attr("transform", d => `
+                            rotate(${d.x * 180 / Math.PI - 90})
+                            translate(${d.y},0)
+                            scale(${(Math.abs(4-d.depth)+1)*2})
+                        `)
+                        .attr("fill", d => d.children ? "#555" : "#999")
+                        .attr('stroke', function(d) { return applyColor(d); })
+                        .attr("r", 0.3)
+                    .attr("cursor", "pointer")
+                    .on('mouseover', function(d,node){
+                        var code_info = node.data["https://www.sdir.no/SFI-model#code"][0]["@value"]
+                        var label_info = node.data["http://www.w3.org/2000/01/rdf-schema#label"][0]["@value"]
+
+                        var labels = code_info + " " + label_info
                     
-                    .attr('stroke', function(d) { return applyColor(d.source); });
+                        var xy = d3.pointer(d, node);
 
-            
-            // Applying attributes to Nodes in graph
-            svg.append("g")
-                .attr('transform', "translate("+(width/2)+","+(height/2)+")")
-                .selectAll("circle")
-                .data(root.descendants())
-                .join("circle")
-                    .attr("transform", d => `
-                        rotate(${d.x * 180 / Math.PI - 90})
-                        translate(${d.y},0)
-                        scale(${(Math.abs(4-d.depth)+1)*2})
-                    `)
-                    .attr("fill", d => d.children ? "#555" : "#999")
-                    .attr('stroke', function(d) { return applyColor(d); })
-                    .attr("r", 0.3)
-                .on('mouseover', function(d,node){
-                    var code_info = node.data["https://www.sdir.no/SFI-model#code"][0]["@value"]
-                    var label_info = node.data["http://www.w3.org/2000/01/rdf-schema#label"][0]["@value"]
+                        div.transition()		
+                            .duration(100)		
+                            .style("visibility", "visible");		
 
-                    var labels = code_info + " " + label_info
-                
-                    var xy = d3.pointer(d, node);
+                        div.html(labels)    
+                            .style("left", xy[0] + 10 + "px")		
+                            .style("top", (xy[1] - 65) + "px")	
+                            .style("position", "absolute")
+                            .style("border-radius", "25px")
+                            .style("padding", "5px")
+                            .style("display", "block")
+                            .style("background-color", "#e7e7e7")
+                            .style("visibility", "visible");		
 
-                    div.transition()		
-                        .duration(300)		
-                        .style("visibility", "visible");		
-
-                    div.html(labels)    
-                        .style("left", xy[0] + "px")		
-                        .style("top", (xy[1] - 28) + "px")	
-                        .style("position", "absolute")
-                        .style("border-radius", "25px")
-                        .style("padding", "5px")
-                        .style("display", "block")
-                        .style("background-color", "#e7e7e7")
-                        .style("visibility", "visible");		
-
-                    })					
-                .on("mouseout", function(d) {		
-                    div.transition()		
-                        .duration(300)		
-                        .style("visibility", "hidden");	
-                })
-                .on("click", collapseLevel);
-                                    
-            
-
-            //Apply attributes to text in graph
-            svg.append("g") 
-                    .attr('transform', "translate("+(width/2)+","+(height/2)+")")
-                    .attr("font-family", "sans-serif")
-                    .attr("font-size", 1.5)
-                    .attr("stroke-linejoin", "round")
-                    .attr("stroke-width", 1.5)
-                .selectAll("text")
-                .data(root.descendants())
-                .join("text")
-                    .attr("transform", d => `
-                        rotate(${d.x * 180 / Math.PI - 90}) 
-                        translate(${d.y},0) 
-                        rotate(${d.x >= Math.PI ? 180 : 0})
-                        scale(${(Math.abs(4-d.depth)+1)*2})
-                    `)
-                    .attr("dy", "0.31em")
-                    .attr("x", d => d.x < Math.PI === !d.children ? 6 : -6)
-                    .attr("text-anchor", d => d.x < Math.PI === !d.children ? "start" : "end")
-                    .text(d => d.data["https://www.sdir.no/SFI-model#code"][0]["@value"])
-                //.text(d => d.data["http://www.w3.org/2000/01/rdf-schema#label"][0]["@value"])
-                .style("padding","1px")
+                        })					
+                    .on("mouseout", function(d) {		
+                        div.transition()		
+                            .duration(100)		
+                            .style("visibility", "hidden");	
+                    })
+                    .on("click", function(d,y){
+                        svg.transition()
+                            .selectAll("*").remove();
+                        div.transition()		
+                            .duration(100)		
+                            .style("visibility", "hidden");	
+                        collapseLevel(d,y,data);
+                    });
+                                        
                 
 
-        }
-        
-            
-      }, [fromJson]);
+                //Apply attributes to text in graph
+                svg.append("g") 
+                        .attr('transform', "translate("+(width2/2)+","+(height2/2)+")")
+                        .attr("font-family", "sans-serif")
+                        .attr("font-size", 1.5)
+                        .attr("stroke-linejoin", "round")
+                        .attr("stroke-width", 1.5)
+                    .selectAll("text")
+                    .data(root.descendants())
+                    .join("text")
+                        .attr("transform", d => `
+                            rotate(${d.x * 180 / Math.PI - 90}) 
+                            translate(${d.y},0) 
+                            rotate(${d.x >= Math.PI ? 180 : 0})
+                            scale(${(Math.abs(4-d.depth)+1)*2})
+                        `)
+                        .attr("dy", "0.31em")
+                        .attr("x", d => d.x < Math.PI === !d.children ? 6 : -6)
+                        .attr("text-anchor", d => d.x < Math.PI === !d.children ? "start" : "end")
+                        .text(d => d.data["https://www.sdir.no/SFI-model#code"][0]["@value"])
+                    //.text(d => d.data["http://www.w3.org/2000/01/rdf-schema#label"][0]["@value"])
+                    .style("padding","1px")
+            }
 
+            update(hierarchy)
+
+            function collapseLevel(event,d,data){
+                console.log(data)
+                if (d.children) {
+                    d._children = d.children;
+                    d.children = null;
+                  } else {
+                    d.children = d._children;
+                    d._children = null;
+                  }
+                
+                update(data);
+                
+            }
             
+        })
+    })
+      
             
     return (
            <div ref={d3KGraph}></div>
